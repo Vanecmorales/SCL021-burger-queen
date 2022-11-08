cfa9dbe0ec7aac222f56cb225b6b33e92022b3fa
Vanecmorales <vanessagm.91@gmail.com>
2022-11-08 13:17:45 -0300

    estilos de la pág de nuevas órdenes

--- burgerqueen/src/pages/OrderPage.js
+++ burgerqueen/src/pages/OrderPage.js
@@ -1,14 +1,19 @@
 import Nav from "../components/Nav";
 export const OrderPage = () => {
   return (
-    <div>
+    <div className="orderPageContainer">
       <Nav/>
       <div className="sectionsContainer">
-        <section className="menu">
+        <section className="menuContainer">
           <button className="breakfastButton">Desayuno</button>
           <button className="mealsButton">Comidas</button>
+          <section className="menuDetails">
+            {/* acá va la info del .json */}
+          </section>
         </section>
-        <section className="orderDetails"></section>
+        <section className="orderDetailsContainer">
+          {/* acá va el detalle de la orden */}
+        </section>
       </div>
     </div>
   );
