49197ec40b89daf96e791c49351bf25f796ca60f
Vanecmorales <vanessagm.91@gmail.com>
2022-11-10 08:09:29 -0300

    Impresión de la data del json

--- burgerqueen/src/pages/OrderPage.js
+++ burgerqueen/src/pages/OrderPage.js
@@ -1,14 +1,35 @@
 import Nav from "../components/Nav";
+import { useState } from "react";
+import menu from "../data/menu.json";
+
+const breakfast = menu.breakfast;
+const lunch = menu.lunch;
+
 export const OrderPage = () => {
+  const [items, setItems] = useState([]);
   return (
     <div className="orderPageContainer">
-      <Nav/>
+      <Nav />
       <div className="sectionsContainer">
         <section className="menuContainer">
-          <button className="breakfastButton">Desayuno</button>
-          <button className="mealsButton">Comidas</button>
+          <button onClick={() => {setItems(breakfast);}}
+            className="breakfastButton">Desayuno</button>
+          <button onClick={() => {setItems(lunch); }}
+            className="mealsButton">Comidas</button>
+
           <section className="menuDetails">
-            {/* acá va la info del .json */}
+            {items.map((item) => (
+              <div className="item" key={item.id}>
+                <h4 className="itemName">{item.name}</h4>
+                <img src={item.image} className="itemImage" alt=""></img>
+
+                <div className="priceButtonContainer">
+                  <h4 className="itemPrice">{"$ " + item.price}</h4>
+                  <button className="addButton">Agregar</button>
+                </div>
+
+              </div>
+            ))}
           </section>
         </section>
         <section className="orderDetailsContainer">
